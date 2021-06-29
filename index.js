d627ebcc8ba5671948715e674660c0e95febf245
Mostafa Mosbah <mmosbah195@gmail.com>
2021-06-29 10:54:05 -0700

    commit tree cd1a6d1a42ea7e0cc96893f09e00dae388329548

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distanceFeet) {
+  let scuberFare;
+  
+  if (distanceFeet <= 400) {
+    scuberFare = "This one is on me!";
+    return scuberFare;
+  } else if (distanceFeet > 2000 && distanceFeet <= 2500) {
+    scuberFare = "I will gladly take your thirty bucks.";
+    return scuberFare;
+  } else if (distanceFeet > 2500) {
+    scuberFare = "No can do.";
+    return scuberFare;
+  }
+}; 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function ternaryCheckCity(city){
+  const isCity =  (city === "NYC") ? "Ok, sounds good." : "No go."
+  return isCity;
+};
+
+function switchOnCharmFromTip(tip){
+  const fareTip = tip
+  switch (fareTip) {
+    case 'generous' : return ("Thank you so much.")
+    break; 
+    case 'not as generous' : return ('Thank you.')
+    break;
+    case 'thanks for everything' : return ('Bye.')
+  }
+} 
+
